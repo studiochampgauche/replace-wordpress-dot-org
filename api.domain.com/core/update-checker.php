bb9a8f295fa968de2d37769930002feb0e87fdda
Studio Champ Gauche <comptes@champgauche.studio>
2024-12-22 15:42:56 -0500

    Make sure its POST Method

--- api.domain.com/core/update-checker.php
+++ api.domain.com/core/update-checker.php
@@ -6,6 +6,13 @@
 
 require_once '../admin/wp-load.php';
 
+if(!isset($_SERVER['REQUEST_METHOD']) || $_SERVER['REQUEST_METHOD'] !== 'POST'){
+
+	wp_send_json_error(null, 405);
+
+	exit;
+
+}
 
 echo wp_json_encode([
 	'offers' => [],
