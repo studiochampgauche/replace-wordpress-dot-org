1b5c7c3f1c641f150e896ffc1b16ec2c0b2ac399
Studio Champ Gauche <comptes@champgauche.studio>
2025-01-12 07:10:37 -0500

    Update update-checker.php

--- api.domain.com/plugins/update-checker.php
+++ api.domain.com/plugins/update-checker.php
@@ -17,7 +17,10 @@ if(!isset($_SERVER['REQUEST_METHOD']) || $_SERVER['REQUEST_METHOD'] !== 'POST'){
 
 
 $databaseURL = 'http://raw.githubusercontent.com/studiochampgauche/replace-wordpress-dot-org/master/api.domain.com/plugins/database/';
-$databaseURLs = [];
+
+$mh = curl_multi_init();
+$curl_handles = [];
+
 
 $pluginsData = json_decode(stripslashes($_POST['plugins']), true);
 $active_plugins = array_intersect_key($pluginsData['plugins'], array_flip($pluginsData['active']));
@@ -30,15 +33,48 @@ $return = [
 
 if($active_plugins){
 
+	$x = 0;
 	foreach ($active_plugins as $k => $v) {
 		
 		if($v['UpdateURI']) continue;
 
 		$slug = explode('/', $k)[0];
 
-		$databaseURLs[] = $databaseURL . $slug . '.json';
+		$url = $databaseURL . $slug . '.json';
+
+		$curl_handles[$x] = curl_init($url);
+	    curl_setopt($curl_handles[$x], CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($curl_handles[$x], CURLOPT_FOLLOWLOCATION, true);
+	    curl_multi_add_handle($mh, $curl_handles[$x]);
+
+		$x++;
+	}
+
+	do {
+		$status = curl_multi_exec($mh, $active);
+	} while ($active);
+
+	$responses = [];
+	foreach ($curl_handles as $i => $ch) {
+	    $responses[$i] = json_decode(curl_multi_getcontent($ch), true);
+	    curl_multi_remove_handle($mh, $ch);
+
+
+	   	if($responses[$i]['version'] > array_values($active_plugins)[$i]['Version']){
+
+	   		$return['plugins'][] = $responses[$i];
+
+	   	} else {
+
+	   		$return['no_update'][] = $responses[$i];
+
+	   	}
+
+
 
 	}
+
+	curl_multi_close($mh);
 
 }
 
