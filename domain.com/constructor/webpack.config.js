fb27eb28432bda40aaa6bf67eed9052a3a84ae55
Studio Champ Gauche <comptes@champgauche.studio>
2025-02-26 12:26:29 -0500

    Remove structure for domain.com
    it will be replace by the new version of ReactWP

--- domain.com/constructor/webpack.config.js
+++
@@ -1,9 +0,0 @@
-import themes from './webpack.themes.config.js';
-import plugins from './webpack.plugins.config.js';
-import muPlugins from './webpack.mu-plugins.config.js';
-
-export default [
-	themes,
-	plugins,
-	muPlugins
-];
--- domain.com/src/themes/reactwp/template/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-get_header();
-while(have_posts()) : the_post();
-
-	
-
-endwhile; get_footer(); ?>
